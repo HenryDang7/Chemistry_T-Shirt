b9e8f4f066981311f4622e9f9252c5c36d8362ed
Henry <113313871+HenryDang7@users.noreply.github.com>
2023-02-23 07:50:20 -0600

    Updated Code

--- Chemistry T-Shirt.ino
+++ Chemistry T-Shirt.ino
@@ -55,7 +55,7 @@ void setup() {
   bottomDisplay.setTextAlignment(PA_CENTER);
 
   topDisplay.print("Gallium");
-  bottomDisplay.displayScroll("Electron Configuration: 1s^2 2s^2 2p^6 3s^2 3p^6 4s^2 3d^10 4p^1", PA_CENTER, PA_SCROLL_LEFT, 25);
+  bottomDisplay.displayScroll("The Coolest Metal :D", PA_CENTER, PA_SCROLL_LEFT, 25);
 }
 
 void loop() {
@@ -64,35 +64,60 @@ void loop() {
     previouslyActivated = true;
     currentState++;
 
-    currentState %= 6;
+    currentState %= 11;
 
     switch(currentState) {
       case 0:
         bottomDisplay.displayClear();
-        bottomDisplay.displayScroll("Electron Configuration: 1s^2 2s^2 2p^6 3s^2 3p^6 4s^2 3d^10 4p^1", PA_CENTER, PA_SCROLL_LEFT, 25);
+        bottomDisplay.displayScroll("The Coolest Metal :D", PA_CENTER, PA_SCROLL_LEFT, 25);
         break;
       
       case 1:
         bottomDisplay.displayClear();
-        bottomDisplay.displayScroll("Uses: LED Lights, Circuits, Switches, and much more!", PA_CENTER, PA_SCROLL_LEFT, 25);
+        bottomDisplay.displayScroll("Part of the Boron Family", PA_CENTER, PA_SCROLL_LEFT, 25);
         break;
-
+      
       case 2:
         bottomDisplay.displayClear();
-        bottomDisplay.displayScroll("I am going to lose it", PA_CENTER, PA_SCROLL_LEFT, 25);
+        bottomDisplay.displayScroll("Electron Configuration: 1s^2 2s^2 2p^6 3s^2 3p^6 4s^2 3d^10 4p^1", PA_CENTER, PA_SCROLL_LEFT, 25);
         break;
       
       case 3:
         bottomDisplay.displayClear();
+        bottomDisplay.displayScroll("Two Stable Isotopes: Ga-69 (68.9256) and Ga-71 (70.9247)", PA_CENTER, PA_SCROLL_LEFT, 25);
+        break;
+
+      case 4:
+        bottomDisplay.displayClear();
+        bottomDisplay.displayScroll("Oxidation Number: +3", PA_CENTER, PA_SCROLL_LEFT, 25);
+        break;
+      
+      case 5:
+        bottomDisplay.displayClear();
+        bottomDisplay.displayScroll("Discovered in 1875 by Paul-Emile Lecoq de Boisbaudran (A French Chemist!)", PA_CENTER, PA_SCROLL_LEFT, 25);
+        break;
+
+      case 6:
+        bottomDisplay.displayClear();
+        bottomDisplay.displayScroll("Everyday Uses: LED Lights, Circuits, Switches, and much more!", PA_CENTER, PA_SCROLL_LEFT, 25);
+        break;
+
+      case 7:
+        bottomDisplay.displayClear();
+        bottomDisplay.displayScroll("I am going to lose it", PA_CENTER, PA_SCROLL_LEFT, 25);
+        break;
+      
+      case 8:
+        bottomDisplay.displayClear();
         bottomDisplay.displayScroll("AAAAHHHHHHHHH", PA_CENTER, PA_SCROLL_LEFT, 0);
         break;
       
-      case 4:
+      case 9:
         bottomDisplay.displayClear();
         bottomDisplay.displayScroll("I love Kris Vazquez, Athena Aguilera, Alejandro Hernandez Espinoza, Krisha Rupani, and so many others!", PA_CENTER, PA_SCROLL_LEFT, 0);
         break;
       
-      case 5:
+      case 10:
         bottomDisplay.displayClear();
         bottomDisplay.setTextAlignment(PA_CENTER);
         startTimer();
@@ -103,7 +128,7 @@ void loop() {
   }
 
   // Update Animations
-  if (bottomDisplay.displayAnimate() && currentState != 5) {
+  if (bottomDisplay.displayAnimate() && currentState != 10) {
     bottomDisplay.displayReset();
   }
 
